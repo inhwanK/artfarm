dbee8c4978872000f0754d950404e5f8eb0c07c7
inhwanK <inhan1009@naver.com>
2022-01-07 10:50:49 +0900

    pathvariable 수정.

--- src/main/java/org/hustar/artfarm/controller/RestNoticeController.java
+++ src/main/java/org/hustar/artfarm/controller/RestNoticeController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
