a5d961e20a39bd14b9b999d807bfb9e45b01fcbc
inhwanK <inhan1009@naver.com>
2022-01-11 20:10:44 +0900

    front ip 외부 properties로 추가 완료.

--- src/main/java/org/hustar/artfarm/config/WebMvcConfig.java
+++ src/main/java/org/hustar/artfarm/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package org.hustar.artfarm.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,13 +22,19 @@ public class WebMvcConfig extends WebSecurityConfigurerAdapter {
 
 	private final long MAX_AGE_SECS = 3600;
 	
+	@Value("${inpyo.ip}")
+	private final String frontIp;
+	
+	@Value("${inhwan.ip}")
+	private final String testIp;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.addAllowedOrigin("http://192.168.0.14:8080");
-		configuration.addAllowedOrigin("http://10.1.4.106:8082");
+		configuration.addAllowedOrigin(frontIp);
+		configuration.addAllowedOrigin(testIp);
 		
 		configuration.addAllowedMethod("GET");
 		configuration.addAllowedMethod("POST");
