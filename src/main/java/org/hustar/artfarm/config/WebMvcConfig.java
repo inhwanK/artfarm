0af8edd7023d8cb20e864f058b2ed6dc74fac8ea
inhwanK <inhan1009@naver.com>
2022-01-11 19:52:09 +0900

    cors 해결, 프론트 서버 ip 할당.

--- src/main/java/org/hustar/artfarm/config/WebMvcConfig.java
+++ src/main/java/org/hustar/artfarm/config/WebMvcConfig.java
@@ -11,21 +11,24 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
+//api 요청 권한 체크를 위한 어노테이션
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
+
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class WebMvcConfig extends WebSecurityConfigurerAdapter {
 
 	private final long MAX_AGE_SECS = 3600;
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		CorsConfiguration configuration = new CorsConfiguration();
 
 		configuration.addAllowedOrigin("http://192.168.0.14:8080");
-
+		configuration.addAllowedOrigin("http://10.1.4.106:8082");
+		
 		configuration.addAllowedMethod("GET");
 		configuration.addAllowedMethod("POST");
 		configuration.addAllowedMethod("PUT");
