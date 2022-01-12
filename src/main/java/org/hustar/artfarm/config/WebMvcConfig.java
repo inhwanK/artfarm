1695b4aabd13c9822d33d5d7bb1b34cf12c7cf62
inhwanK <inhan1009@naver.com>
2022-01-12 11:35:50 +0900

    config 수정.

--- src/main/java/org/hustar/artfarm/config/WebMvcConfig.java
+++ src/main/java/org/hustar/artfarm/config/WebMvcConfig.java
@@ -1,12 +1,13 @@
 package org.hustar.artfarm.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 // 임포트 패키지 주의.
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
@@ -19,34 +20,10 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 public class WebMvcConfig extends WebSecurityConfigurerAdapter {
 
-	private final long MAX_AGE_SECS = 3600;
-	
-	
-	private final String frontIp = "http://10.1.4.106:8082";
-	private final String testIp = "http://192.168.0.14:8080";
-	
+	private final long MAX_AGE_SECS = 3600;	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {		
-		CorsConfiguration configuration = new CorsConfiguration();
-
-		System.out.println("testIp > " +testIp);
-		
-		configuration.addAllowedOrigin(frontIp);
-		configuration.addAllowedOrigin(testIp);
-		
-		configuration.addAllowedMethod("GET");
-		configuration.addAllowedMethod("POST");
-		configuration.addAllowedMethod("PUT");
-		configuration.addAllowedMethod("PATCH");
-		configuration.addAllowedMethod("DELETE");
-		configuration.addAllowedMethod("OPTIONS");
-
-		configuration.addAllowedHeader("*");
-		configuration.setMaxAge(MAX_AGE_SECS);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
 
 		http
 			.csrf().disable()
@@ -56,6 +33,25 @@ public class WebMvcConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers().authenticated()
 			.anyRequest().permitAll()
 		.and()
-			.cors().configurationSource(source);
+			.cors();
+	}
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+
+		CorsConfiguration configuration = new CorsConfiguration();
+		
+		configuration.addAllowedOrigin("http://10.1.4.106:8082");
+		configuration.addAllowedOrigin("http://192.168.0.14:8080");
+		
+		System.out.println("origins > "+configuration.getAllowedOrigins());
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+		configuration.setMaxAge(MAX_AGE_SECS);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
 	}
 }
