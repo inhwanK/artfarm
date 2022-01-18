c589ed956526bec326fcf6a6aca1baec9f1b718b
inhwanK <inhan1009@naver.com>
2022-01-19 01:35:51 +0900

    s3 파일 경로 정하는 중.

--- src/main/java/org/hustar/artfarm/controller/RestFileController.java
+++ src/main/java/org/hustar/artfarm/controller/RestFileController.java
@@ -3,6 +3,7 @@ package org.hustar.artfarm.controller;
 import java.io.IOException;
 
 import org.hustar.artfarm.file.S3Uploader;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +18,15 @@ import lombok.RequiredArgsConstructor;
 public class RestFileController {
 
 	private final S3Uploader s3Uploader;
-	
+
+//	img 불러오는 get 통신 해야함.
+	@GetMapping("/getimage")
+	public String download(String fileName) {
+		return "https://artfarm-storage.s3.ap-northeast-2.amazonaws.com/static/" + fileName;
+	}
+
 	@PostMapping("/images")
-	public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException{
+	public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
 		s3Uploader.upload(multipartFile, "static");
 		return "test";
 	}
