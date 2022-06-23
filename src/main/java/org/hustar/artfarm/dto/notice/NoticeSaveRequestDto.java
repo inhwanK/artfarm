9e166e69460138a70183d96d154ff9d8d4c1c9c4
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-06-23 20:49:30 +0900

    Feat(NoticeController) : Swagger Api 태그,요청 파라미터,응답 추가

--- src/main/java/org/hustar/artfarm/dto/notice/NoticeSaveRequestDto.java
+++ src/main/java/org/hustar/artfarm/dto/notice/NoticeSaveRequestDto.java
@@ -1,5 +1,7 @@
 package org.hustar.artfarm.dto.notice;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.hustar.artfarm.domain.notice.Notice;
 
 import lombok.Builder;
@@ -7,13 +9,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@ApiModel(value = "NoticeSaveRequestDto : 공지사항 등록 요청 Dto", description = "공지사항 등록 요청 Dto")
 @Getter
 @Setter
 @NoArgsConstructor
 public class NoticeSaveRequestDto {
 
+	@ApiModelProperty(value = "등록할 공지사항 제목", required = true, example = "제목입니다.")
 	private String title;
+	@ApiModelProperty(value = "등록할 공지사항 내용", required = true, example = "내용은 이런겁니다 알겠습니까")
 	private String content;
+	@ApiModelProperty(value = "등록할 공지사항 작성자", required = true, example = "김인환")
 	private String writer;
 
 	@Builder
